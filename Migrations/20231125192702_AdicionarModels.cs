882f96988598d4ea6c869169c478dca2b0ba310a
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-25 16:27:51 -0300

    Adicionar migrations para os models

---
+++ Migrations/20231125192702_AdicionarModels.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Livraria.Migrations
+{
+    public partial class AdicionarModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PedidoId",
+                table: "Livros",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Carrinho",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrinho", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Senha = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ItensCarrinho",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdLivro = table.Column<int>(type: "int", nullable: false),
+                    IdCarrinho = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensCarrinho", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItensCarrinho_Carrinho_IdCarrinho",
+                        column: x => x.IdCarrinho,
+                        principalTable: "Carrinho",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItensCarrinho_Livros_IdLivro",
+                        column: x => x.IdLivro,
+                        principalTable: "Livros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Avaliacao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Estrelas = table.Column<int>(type: "int", nullable: false),
+                    Comentario = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false),
+                    IdLivro = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Avaliacao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Avaliacao_Livros_IdLivro",
+                        column: x => x.IdLivro,
+                        principalTable: "Livros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Avaliacao_Usuario_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false),
+                    IdCarrinho = table.Column<int>(type: "int", nullable: false),
+                    FormaDePagamento = table.Column<int>(type: "int", nullable: false),
+                    CarrinhoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pedido_Carrinho_CarrinhoId",
+                        column: x => x.CarrinhoId,
+                        principalTable: "Carrinho",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Pedido_Usuario_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_PedidoId",
+                table: "Livros",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacao_IdLivro",
+                table: "Avaliacao",
+                column: "IdLivro");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacao_IdUsuario",
+                table: "Avaliacao",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensCarrinho_IdCarrinho",
+                table: "ItensCarrinho",
+                column: "IdCarrinho");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensCarrinho_IdLivro",
+                table: "ItensCarrinho",
+                column: "IdLivro");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedido_CarrinhoId",
+                table: "Pedido",
+                column: "CarrinhoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedido_IdUsuario",
+                table: "Pedido",
+                column: "IdUsuario");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Livros_Pedido_PedidoId",
+                table: "Livros",
+                column: "PedidoId",
+                principalTable: "Pedido",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Livros_Pedido_PedidoId",
+                table: "Livros");
+
+            migrationBuilder.DropTable(
+                name: "Avaliacao");
+
+            migrationBuilder.DropTable(
+                name: "ItensCarrinho");
+
+            migrationBuilder.DropTable(
+                name: "Pedido");
+
+            migrationBuilder.DropTable(
+                name: "Carrinho");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Livros_PedidoId",
+                table: "Livros");
+
+            migrationBuilder.DropColumn(
+                name: "PedidoId",
+                table: "Livros");
+        }
+    }
+}
