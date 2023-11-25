e8c5dc410804ca41c95f6fd7fc9657d684a2ddd6
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-25 16:52:34 -0300

    Adicionar scripts e imagens

---
+++ wwwroot/js/books.js
@@ -0,0 +1,90 @@
+const book =[
+    {
+        id: 1,
+        nome: "O Homem de Giz",
+        nota: 4.5,
+        tipo: "Aventura",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at quis. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Malesuada pellentesque elit eget gravida. Urna condimentum mattis pellentesque id. Senectus et netus et malesuada. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Est ullamcorper eget nulla facilisi. Volutpat maecenas volutpat blandit aliquam etiam. Id volutpat lacus laoreet non curabitur gravida arcu. Neque aliquam vestibulum morbi blandit cursus risus at. Molestie nunc non blandit massa enim nec dui. Nulla facilisi morbi tempus iaculis urna id volutpat.",
+        vendas: 1045,
+        preco: "15.00",
+        imagem: "./imagens/extraordinario.jpg",
+        autor: "R.J. Palacio"
+    },
+    {
+        id: 2,
+        nome: "Extraordinário",
+        nota: 4.8,
+        tipo: "Aventura",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at quis. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Malesuada pellentesque elit eget gravida. Urna condimentum mattis pellentesque id. Senectus et netus et malesuada. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Est ullamcorper eget nulla facilisi. Volutpat maecenas volutpat blandit aliquam etiam. Id volutpat lacus laoreet non curabitur gravida arcu. Neque aliquam vestibulum morbi blandit cursus risus at. Molestie nunc non blandit massa enim nec dui. Nulla facilisi morbi tempus iaculis urna id volutpat.",
+        vendas: 1234,
+        preco: "18.00",
+        imagem: "./imagens/o-homem-de-giz.jpg",
+        autor: "C.J. Tudor"
+    },
+    {
+        id: 3,
+        nome: "Biblia",
+        nota: 5,
+        tipo: "Aventura",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at quis. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Malesuada pellentesque elit eget gravida. Urna condimentum mattis pellentesque id. Senectus et netus et malesuada. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Est ullamcorper eget nulla facilisi. Volutpat maecenas volutpat blandit aliquam etiam. Id volutpat lacus laoreet non curabitur gravida arcu. Neque aliquam vestibulum morbi blandit cursus risus at. Molestie nunc non blandit massa enim nec dui. Nulla facilisi morbi tempus iaculis urna id volutpat.",
+        vendas: 16434,
+        preco: "20.00",
+        imagem: "./imagens/biblia.png",
+        autor: "R.J. Palacio"
+    },
+    {
+        id: 4,
+        nome: "O Menor que se fez Maior",
+        nota: 4.4,
+        tipo: "Aventura",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at quis. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Malesuada pellentesque elit eget gravida. Urna condimentum mattis pellentesque id. Senectus et netus et malesuada. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Est ullamcorper eget nulla facilisi. Volutpat maecenas volutpat blandit aliquam etiam. Id volutpat lacus laoreet non curabitur gravida arcu. Neque aliquam vestibulum morbi blandit cursus risus at. Molestie nunc non blandit massa enim nec dui. Nulla facilisi morbi tempus iaculis urna id volutpat.",
+        vendas: 2345,
+        preco: "18.00",
+        imagem: "./imagens/o-menor-que-se-fez-maior.jpg",
+        autor: "C.J. Tudor"
+    },
+    {
+        id: 5,
+        nome: "O Contador de Histórias",
+        nota: 4.2,
+        tipo: "Aventura",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at quis. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Malesuada pellentesque elit eget gravida. Urna condimentum mattis pellentesque id. Senectus et netus et malesuada. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Est ullamcorper eget nulla facilisi. Volutpat maecenas volutpat blandit aliquam etiam. Id volutpat lacus laoreet non curabitur gravida arcu. Neque aliquam vestibulum morbi blandit cursus risus at. Molestie nunc non blandit massa enim nec dui. Nulla facilisi morbi tempus iaculis urna id volutpat.",
+        vendas: 1045,
+        preco: "15.00",
+        imagem: "./imagens/o-contador-de-historias.jpg",
+        autor: "R.J. Palacio"
+    },
+    {
+        id: 6,
+        nome: "Alcorão",
+        nota: 4.4,
+        tipo: "Aventura",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at quis. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Malesuada pellentesque elit eget gravida. Urna condimentum mattis pellentesque id. Senectus et netus et malesuada. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Est ullamcorper eget nulla facilisi. Volutpat maecenas volutpat blandit aliquam etiam. Id volutpat lacus laoreet non curabitur gravida arcu. Neque aliquam vestibulum morbi blandit cursus risus at. Molestie nunc non blandit massa enim nec dui. Nulla facilisi morbi tempus iaculis urna id volutpat.",
+        vendas: 1234,
+        preco: "18.00",
+        imagem: "./imagens/o-alcorao.jpg",
+        autor: "C.J. Tudor"
+    },
+    {
+        id: 7,
+        nome: "Revolução dos Bichos",
+        nota: 4.8,
+        tipo: "Aventura",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at quis. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Malesuada pellentesque elit eget gravida. Urna condimentum mattis pellentesque id. Senectus et netus et malesuada. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Est ullamcorper eget nulla facilisi. Volutpat maecenas volutpat blandit aliquam etiam. Id volutpat lacus laoreet non curabitur gravida arcu. Neque aliquam vestibulum morbi blandit cursus risus at. Molestie nunc non blandit massa enim nec dui. Nulla facilisi morbi tempus iaculis urna id volutpat.",
+        vendas: 1644,
+        preco: "15.00",
+        imagem: "./imagens/revolucao-dos-bichos.jpeg",
+        autor: "R.J. Palacio"
+    },
+    {
+        id: 8,
+        nome: "A morte e a morte de Quincas Berro d'Agua",
+        nota: 2,
+        tipo: "Aventura",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non odio euismod lacinia at quis. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Malesuada pellentesque elit eget gravida. Urna condimentum mattis pellentesque id. Senectus et netus et malesuada. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Est ullamcorper eget nulla facilisi. Volutpat maecenas volutpat blandit aliquam etiam. Id volutpat lacus laoreet non curabitur gravida arcu. Neque aliquam vestibulum morbi blandit cursus risus at. Molestie nunc non blandit massa enim nec dui. Nulla facilisi morbi tempus iaculis urna id volutpat.",
+        vendas: 22,
+        preco: "18.00",
+        imagem: "./imagens/a-morte-e-a-morte-de-quincas-berro-dagua.jpg",
+        autor: "C.J. Tudor"
+    },
+]
---
+++ wwwroot/js/index.js
@@ -0,0 +1,38 @@
+function ordenarPorVenda(a, b) {
+    return b.vendas - a.vendas;
+}
+function ordenarPorNota(a, b) {
+    return b.nota - a.nota;
+}
+
+
+if (book !== undefined) {
+    console.log("Ordenando livros");
+    book.sort(ordenarPorNota);
+} else {
+    console.error("Não temos livros");
+}
+
+function listarBookVendas(id, array) {
+    let span = document.getElementById(id);
+    span.innerHTML = '';
+
+    for (let i = 0; i < 4; i++) {
+        span.innerHTML += '<div class="book-container"><img src="' + array[i].imagem + '"><div style="padding: 15px;"><h5>' + array[i].nome + '</h5><p>' + array[i].autor + '</p><br><div style="position: absolute; top: 80%;"><p> R$ ' + array[i].preco + '</p><button onclick="getData(' + array[i].id + ')" >Ver Item</button></div></div>'
+    }
+}
+listarBookVendas('mais-vendidos', book);
+
+if (book !== undefined) {
+    console.log("Ordenando livros");
+    book.sort(ordenarPorNota);
+} else {
+    console.error("Não temos livros");
+}
+
+listarBookVendas('mais-avaliados', book);
+
+function getData(i) {
+    localStorage.setItem("value", i);
+    window.location.href = "produto.html";
+}
