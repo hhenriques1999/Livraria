521c2dfb728b3aa752d73fd9f42f18475a2c64cb
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-25 16:20:53 -0300

    Add project files.

---
+++ LivrariaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Livraria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Livraria
+{
+    public class LivrariaDbContext : DbContext
+    {
+        public LivrariaDbContext(DbContextOptions<LivrariaDbContext> options) : base(options) { }
+        public DbSet<Livro> Livros { get; set; }
+    }
+
+}
---
+++ Models/Livro.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Livraria.Models
+{
+    public class Livro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public float Preco { get; set; }
+    }
+}
