c636f13afcf68f62353466fa9f2e05a5579023f4
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-25 17:47:16 -0300

    Adicionar qtd vendas livro

---
+++ Migrations/20231125204649_QtdVendasLivro.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Livraria.Migrations
+{
+    public partial class QtdVendasLivro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Livros",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Capa",
+                table: "Livros",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "QtdVendas",
+                table: "Livros",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QtdVendas",
+                table: "Livros");
+
+            migrationBuilder.UpdateData(
+                table: "Livros",
+                keyColumn: "Nome",
+                keyValue: null,
+                column: "Nome",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Livros",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Livros",
+                keyColumn: "Capa",
+                keyValue: null,
+                column: "Capa",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Capa",
+                table: "Livros",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- Models/Livro.cs
+++ Models/Livro.cs
@@ -8,7 +8,9 @@ namespace Livraria.Models
         public int Id { get; set; }
         public string? Nome { get; set; }
         public float Preco { get; set; }
-        public string? Capa { get;set; }
+        public string? Capa { get; set; }
+        [Display(Name = "Quantidade de Vendas")]
+        public int QtdVendas { get; set; }
         public virtual List<Avaliacao>? Avaliacoes { get; set; }
     }
 }
