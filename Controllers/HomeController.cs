b2292adf3cc25b6817224fd5b2168245a1afe8fd
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-26 13:45:43 -0300

    Acertar estilos do header e incluir funcionalidade 'Sobre Nós'

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Livraria.Controllers
             return View();
         }
 
+        public IActionResult SobreNos()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
