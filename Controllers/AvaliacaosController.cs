1d70258fb646b9d3677c6729d2123b4d47997eb7
hhenriques1999 <hhenriques1999@gmail.com>
2023-11-26 16:59:44 -0300

    Criar controller de avaliacao

---
+++ Controllers/AvaliacaosController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Livraria.Data;
+using Livraria.Models;
+
+namespace Livraria.Controllers
+{
+    public class AvaliacaosController : Controller
+    {
+        private readonly LivrariaDbContext _context;
+
+        public AvaliacaosController(LivrariaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Avaliacaos
+        public async Task<IActionResult> Index()
+        {
+            var livrariaDbContext = _context.Avaliacao.Include(a => a.Avaliador).Include(a => a.Livro);
+            return View(await livrariaDbContext.ToListAsync());
+        }
+
+        // GET: Avaliacaos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacao
+                .Include(a => a.Avaliador)
+                .Include(a => a.Livro)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacaos/Create
+        public IActionResult Create()
+        {
+            ViewData["IdUsuario"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["IdLivro"] = new SelectList(_context.Livros, "Id", "Id");
+            return View();
+        }
+
+        // POST: Avaliacaos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Estrelas,Comentario,IdUsuario,IdLivro")] Avaliacao avaliacao)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(avaliacao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdUsuario"] = new SelectList(_context.Users, "Id", "Id", avaliacao.IdUsuario);
+            ViewData["IdLivro"] = new SelectList(_context.Livros, "Id", "Id", avaliacao.IdLivro);
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacaos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacao.FindAsync(id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdUsuario"] = new SelectList(_context.Users, "Id", "Id", avaliacao.IdUsuario);
+            ViewData["IdLivro"] = new SelectList(_context.Livros, "Id", "Id", avaliacao.IdLivro);
+            return View(avaliacao);
+        }
+
+        // POST: Avaliacaos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Estrelas,Comentario,IdUsuario,IdLivro")] Avaliacao avaliacao)
+        {
+            if (id != avaliacao.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(avaliacao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AvaliacaoExists(avaliacao.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdUsuario"] = new SelectList(_context.Users, "Id", "Id", avaliacao.IdUsuario);
+            ViewData["IdLivro"] = new SelectList(_context.Livros, "Id", "Id", avaliacao.IdLivro);
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacaos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacao
+                .Include(a => a.Avaliador)
+                .Include(a => a.Livro)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(avaliacao);
+        }
+
+        // POST: Avaliacaos/Delete/5
+        [HttpPost("Delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (_context.Avaliacao == null)
+            {
+                return Problem("Entity set 'LivrariaDbContext.Avaliacao'  is null.");
+            }
+            var avaliacao = await _context.Avaliacao.FindAsync(id);
+            if (avaliacao != null)
+            {
+                _context.Avaliacao.Remove(avaliacao);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool AvaliacaoExists(int id)
+        {
+          return (_context.Avaliacao?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
